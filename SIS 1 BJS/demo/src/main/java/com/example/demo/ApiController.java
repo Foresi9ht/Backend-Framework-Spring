a273c3a74fae6eced7986930aff3569ca6d542d6
Foresi9ht <157278731+Foresi9ht@users.noreply.github.com>
2026-02-12 11:41:31 +0500

    ApiController.java

--- SIS 1 BJS/demo/src/main/java/com/example/demo/ApiController.java
+++ SIS 1 BJS/demo/src/main/java/com/example/demo/ApiController.java
@@ -6,26 +6,23 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api")
 public class ApiController {
-
-    // GET endpoint
     @GetMapping("/info")
     public Map<String, Object> getInfo() {
         return Map.of(
-                "student", "Sultik",  // напиши своё имя если нужно
+                "student", "Sultik",
                 "course", "Spring Framework",
                 "week", 1
         );
     }
-
-    // POST endpoint
     @PostMapping("/calculate")
     public Map<String, Integer> calculate(@RequestBody Map<String, Integer> body) {
 
         int a = body.get("a");
         int b = body.get("b");
 
-        int result = a + b; // сложение
+        int result = a + b;
 
         return Map.of("result", result);
     }
 }
+
